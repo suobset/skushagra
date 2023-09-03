ed690065206bfd8d13b051a1f0fc046008e76c52
Kush Srivastava <kushagra2410@gmail.com>
2023-09-02 20:07:44 -0400

    Fix docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://skushagra.com',
+  url: 'https://suobset.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/skushagra',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
