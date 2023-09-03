6759401d7563f1fbcfe2241620186703f404a205
Kush Srivastava <kushagra2410@gmail.com>
2023-09-02 22:58:06 -0400

    Try changing config to no baseUrl now that it's like on main URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://suobset.github.io',
+  url: 'https://skushagra.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/skushagra',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
