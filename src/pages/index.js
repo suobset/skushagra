01255f2ea248db64f02f7c1a1420780e538db589
Kushagra Srivastava <kushagra2410@gmail.com>
2023-09-02 23:10:56 -0400

    Add link to old webpage for visitors

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://suobset.github.io/archive/web_v2">
+            Visit Old Webpage
           </Link>
         </div>
       </div>
