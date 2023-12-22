832782eed7a0ff94b3e156cef94d8740fe21cae7
Kush Srivastava <kushagra2410@gmail.com>
2023-12-22 08:42:08 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Kushagra Srivastava',
-  tagline: 'Operating Systems & Compilers Programmer | Undergrad',
+  tagline: 'Operating Systems & Compilers Programmer | AI/ML Specialization',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
