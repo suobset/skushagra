9d72343f7eec9a8d403e94aba8f019162426854c
kushagra2410 <kushagra2410@gmail.com>
2025-02-20 16:34:01 +0000

    Commit IDX config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,9 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.neofetch
   ];
 
   # Sets environment variables in the workspace
